4343e417d6ec58fb08cefad55edeb2722436efe9
Vanna <vhluu@ucsd.edu>
2018-12-21 18:40:08 -0800

    fixed urls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  Route,
-  HashRouter
-} from "react-router-dom";
 import Intro from './Intro';
-import Main from './Main';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="content">
-          <Route exact path="/" component={Intro} />
-          <Route path="/main" component={Main} />
-        </div>
-      </HashRouter>
+      <Intro/>
     );
   }
 }
