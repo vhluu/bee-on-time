a73f275aa3d45f43b511fa9a3a7a538729d0330b
Vanna Luu <vhluu@ucsd.edu>
2021-01-12 12:42:12 -0800

    created button and modal components

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import '../App.css';
 
 class Clock extends Component {
   constructor(props) {
---
+++ src/components/Modal.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Button from './Button';
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside(event) {
+    const { show, closeSelf } = this.props;
+    
+    if (show && event.target.classList.contains('modal')) {
+      if (closeSelf) closeSelf(event);
+    }
+  }
+
+  render() {
+    const { show, children, extraClasses = '', closeSelf } = this.props;
+    return (
+      <div className={`modal ${extraClasses}${show ? '' : ' hide'}`}>
+        <div className="modal-content">
+          <Button btnType="close" clicked={closeSelf} />
+          { children }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
